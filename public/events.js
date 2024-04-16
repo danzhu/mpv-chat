1350b09a0f222a1fe02c63ee01e03afe36e975fd
Daniel Zhu <danzhu24@hotmail.com>
2024-04-16 13:35:42 -0400

    use mutation observer for scrolling

--- public/events.js
+++ public/events.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const content = document.getElementById('content')
+
+    let scroll = false
+
+    const observer = new MutationObserver(() => {
+        if (scroll) {
+            window.scrollTo(0, document.body.scrollHeight)
+        }
+    })
+    observer.observe(content, { childList: true })
+
     const source = new EventSource(document.location.pathname)
     source.addEventListener('message', (event) => {
         const data = JSON.parse(event.data)
         document.title = data.title
         content.innerHTML = data.content
-        if (data.scroll) {
-            window.scrollTo(0, document.body.scrollHeight)
-        }
+        scroll = data.scroll
 
         for (const b of content.querySelectorAll('[data-post]')) {
             b.addEventListener('click', () => {
